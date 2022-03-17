9808e4fa06fd703ac666d658bd1326d8b5b184eb
HEEHWANWANG <67574748+HEEHWANWANG@users.noreply.github.com>
2022-03-17 16:22:21 +0900

    Add files via upload

--- STEP_2_Multitask-learning/3DCNN_hardparameter_sharing/utils/utils.py
+++ STEP_2_Multitask-learning/3DCNN_hardparameter_sharing/utils/utils.py
@@ -60,14 +60,14 @@ def CLIreporter(targets, train_loss, train_acc, val_loss, val_acc):
     var_column = []
     visual_report = {}
     visual_report['Loss (train/val)'] = []
-    visual_report['MSE or ACC (train/val)'] = []
+    visual_report['R2 or ACC (train/val)'] = []
     for label_name in targets:
         var_column.append(label_name)
 
         loss_value = '{:2.2f} / {:2.2f}'.format(train_loss[label_name],val_loss[label_name])
         acc_value = '{:2.2f} / {:2.2f}'.format(train_acc[label_name],val_acc[label_name])
         visual_report['Loss (train/val)'].append(loss_value)
-        visual_report['MSE or ACC (train/val)'].append(acc_value)
+        visual_report['R2 or ACC (train/val)'].append(acc_value)
 
     print(pd.DataFrame(visual_report, index=var_column))
 
