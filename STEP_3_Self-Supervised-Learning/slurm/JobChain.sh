e84634568e336aec66ea6e8923ba5ec6844868ee
HEEHWANWANG <67574748+HEEHWANWANG@users.noreply.github.com>
2022-05-09 19:43:37 +0900

    Update JobChain.sh

--- STEP_3_Self-Supervised-Learning/slurm/JobChain.sh
+++ STEP_3_Self-Supervised-Learning/slurm/JobChain.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
   
-#initial script
-START=$(sbatch --parsable UKB_simCLR.sh)
+
 
 #retraining script
-ID=$(sbatch --parsable --dependency=afterok:${START} UKB_simCLR_retrain.sh)
+ID=$(sbatch --parsable UKB_simCLR_retrain.sh)
 
 for i in $(seq $1)
 do
