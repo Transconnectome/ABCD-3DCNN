8fbf1cda598c609af03e3ca91101acb87b34599c
HEEHWANWANG <67574748+HEEHWANWANG@users.noreply.github.com>
2022-05-09 19:43:12 +0900

    Update retrain_script.sh

--- STEP_3_Self-Supervised-Learning/slurm/retrain_script.sh
+++ STEP_3_Self-Supervised-Learning/slurm/retrain_script.sh
@@ -11,4 +11,4 @@
 
 
 cd  /scratch/connectome/dhkdgmlghks/3DCNN_test/simCLR_UKB
-python3 run_simCLR.py --optim LAMB --lr 1e-3  --epoch 25 --model densenet3D121 --train_batch_size 88  --exp_name UKB_simCLR  --augmentation  RandRotate RandCrop RandFlip RandAdjustContrast RandGaussianSmooth RandGibbsNoise --resize 80 80 80 --accumulation_steps 20 --resume True --checkpoint_dir /scratch/connectome/dhkdgmlghks/3DCNN_test/simCLR_UKB/result/model/densenet3D121_UKB_simCLR_16f2e0.pth  --sbatch True
+python3 run_simCLR.py --optim LAMB --lr 1e-3  --epoch 25 --model densenet3D121 --train_batch_size 88  --exp_name UKB_simCLR  --augmentation  RandRotate RandCrop RandFlip RandAdjustContrast RandGaussianSmooth RandGibbsNoise --resize 80 80 80 --accumulation_steps 20 --resume True --checkpoint_dir /scratch/connectome/dhkdgmlghks/3DCNN_test/simCLR_UKB/result/model/{.pth file}  --sbatch True
