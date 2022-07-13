b8a211020de074b63fbe2c08a4ded31e9b3901b0
HEEHWANWANG <67574748+HEEHWANWANG@users.noreply.github.com>
2022-07-13 17:30:26 +0900

    Add files via upload

---
+++ STEP_3_Self-Supervised-Learning/MAE/model/layers/mlp.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+from .helpers import to_2tuple
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, bias=True, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        bias = to_2tuple(bias)
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias[0])
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias[1])
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
